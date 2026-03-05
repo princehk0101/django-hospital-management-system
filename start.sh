01bd0b594a7f741dd7af96dadedfdb57a09847c6
princehk0101 <chhotelalchauhanhk07@gmail.com>
2026-03-05 17:13:49 +0530

    fix render port binding

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-gunicorn Hospital_Management_System.wsgi:application
+#!/usr/bin/env bash
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+gunicorn Hospital_Management_System.wsgi:application --bind 0.0.0.0:$PORTg
