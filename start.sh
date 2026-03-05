b43bddd464b3a79b575cc04d32904bea7e517727
princehk0101 <chhotelalchauhanhk07@gmail.com>
2026-03-05 23:26:54 +0530

    fix render port variable

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn Hospital_Management_System.wsgi:application --bind 0.0.0.0:$PORTg
+gunicorn Hospital_Management_System.wsgi:application --bind 0.0.0.0:${PORT}
